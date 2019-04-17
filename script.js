36578fc7084485feb40ee649afecdb932c412823
Zachary Schweikert <zach.schweikert@gmail.com>
2019-04-17 12:16:04 -0700

    Huge progress made
    A contact page utilizing PHP has been mostly set up. The projects page has information on it now. The about me page has information but no pictures yet.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 ﻿$(document).ready(function () {
+    
+
+
+    
+
+
+
 
     //////////////////////////////////////////////////////////// initializing variables and listeners
     var isOnEngineering = false;
-    $('#engineeringProjects').mouseenter(function () { isOnEngineering = true; });
-    $('#engineeringProjects').mouseleave(function () { isOnEngineering = false; });
+    $('#engineeringProjects').mouseenter(function () {
+            engineeringFlag = true;
+    });
+    $('#engineeringProjects').mouseleave(function () {
+            engineeringFlag = false;
+    });
     var isOnProgramming = false;
-    $('#programmingProjects').mouseenter(function () { isOnProgramming = true; });
-    $('#programmingProjects').mouseleave(function () { isOnProgramming = false; });
-
+    $('#programmingProjects').mouseenter(function () {
+            programmingFlag = true;
+    });
+    $('#programmingProjects').mouseleave(function () {
+            programmingFlag = false;
+    });
 
 
     ///////////////////////////////////////////////////////////////Fade out and transition to the clicked page
@@ -28,49 +42,70 @@
 
    
     ///////////////////////////////////////////////////////////////////////// window is being scrolled
-    var scrolled = 0;
     var pageHeight = $(document).height();
+    var pageWidth = $(document).width();
     var windowHeight = $(window).height();
+    var lastIsOnEngineering = false;
+    var lastIsOnProgramming = false;
+    var percentPageDown = 0;
+
+    var engineeringFlag = false;
+    var programmingFlag = false;
+
     $(window).scroll(function () {
 
         var st = $(this).scrollTop();
-        var percentPageDown = st / (pageHeight - windowHeight) * 100;
-
+        percentPageDown = st / (pageHeight - windowHeight) * 100;
 
 
         if (percentPageDown < 1) {
-            $('#engineeringProjects').css({ "-webkit-transform": "translate(0px, 0px)" });
-            $('#programmingProjects').css({ "-webkit-transform": "translate(0px, 0px)" });
-            $('#engineeringProjects').fadeTo(0, 1);
-            $('#programmingProjects').fadeTo(0, 1);
+            $('#engineeringProjects').width("50%")
+            $('#programmingProjects').width("50%")
+            $('#programmingProjects').children().width(pageWidth / 2 - pageWidth * .01);
+            $('#engineeringProjects').children().width(pageWidth / 2 - pageWidth * .01);
             return;
         }
 
+
+        if (percentPageDown < 8) {
+            isOnEngineering = engineeringFlag;
+            isOnProgramming = programmingFlag;
+        }
+        
+
+        console.log("engineering=" + isOnEngineering + ", programming" + isOnProgramming + ", %=" + percentPageDown);
+
+
         /////////////// on engineering
-        if (isOnEngineering && percentPageDown < 50 && percentPageDown > 0) {
-            $('#engineeringProjects').css({ "-webkit-transform": "translate(" + percentPageDown + "%, 0px)" });
-            $('#programmingProjects').css({ "-webkit-transform": "translate(" + percentPageDown + "%, 0px)" });
-            $('#programmingProjects').fadeTo(0, 1 - percentPageDown / 100);
+        if (isOnEngineering && percentPageDown < 50) {
+            //console.log((pageWidth / 2) - percentPageDown * 2 / 100 * (pageWidth / 2) )
+            
+
+            $('#programmingProjects').width(50 - percentPageDown + "%");
+            $('#programmingProjects').children().width((pageWidth / 2) - percentPageDown * 2 / 100 * (pageWidth / 2) - pageWidth * .01);
+
+            $('#engineeringProjects').children().width((pageWidth / 2) - pageWidth * .01);
+            $('#engineeringProjects').width(percentPageDown + 50 + "%");
         }
         else if (isOnEngineering && percentPageDown >= 50) {
-            $('#engineeringProjects').css({ "-webkit-transform": "translate(50%, 0px)" });
-            $('#programmingProjects').css({ "-webkit-transform": "translate(50%, 0px)" });
-            $('#engineeringProjects').fadeTo(0, 1);
-            $('#programmingProjects').fadeTo(0, 0);
+            $('#programmingProjects').children().width(0);
+            $('#programmingProjects').width("0px");
+            $('#engineeringProjects').width("100%");
             return;
         }
 
         ///////////// on programming
-        if (isOnProgramming && percentPageDown < 50 && percentPageDown > 0) {
-            $('#programmingProjects').css({ "-webkit-transform": "translate(" + -1 * percentPageDown + "%, 0px)" });
-            $('#engineeringProjects').css({ "-webkit-transform": "translate(" + -1 * percentPageDown + "%, 0px)" });
-            $('#engineeringProjects').fadeTo(0, 1 - percentPageDown / 100);
+        if (isOnProgramming && percentPageDown < 50) {
+            $('#engineeringProjects').width(50 - percentPageDown + "%");
+            $('#engineeringProjects').children().width((pageWidth / 2) - percentPageDown * 2 / 100 * (pageWidth / 2) - pageWidth * .01);
+
+            $('#programmingProjects').children().width((pageWidth / 2) - pageWidth * .01);
+            $('#programmingProjects').width(percentPageDown + 50 + "%");
         }
         else if (isOnProgramming && percentPageDown >= 50) {
-            $('#programmingProjects').css({ "-webkit-transform": "translate(-50%, 0px)" });
-            $('#engineeringProjects').css({ "-webkit-transform": "translate(-50%, 0px)" });
-            $('#programmingProjects').fadeTo(0, 1);
-            $('#engineeringProjects').fadeTo(0, 0);
+            $('#engineeringProjects').children().width(0);
+            $('#engineeringProjects').width("0px");
+            $('#programmingProjects').width("100%");
             return;
         }
 
